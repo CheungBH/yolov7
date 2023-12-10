fc164fb5869d204e570f49791813e5c188c72f08
cheungbh <cheungbh@cheungbhdeMacBook-Pro.local>
2023-12-10 15:16:05 +0800

    add temporal queue for inference

---
+++ temporal/utils.py
@@ -0,0 +1,34 @@
+
+
+class TemporalQueue:
+    def __init__(self, queue_size=5, queue_interval=2):
+        self.queue = []
+        self.queue_size = queue_size
+        self.queue_interval = queue_interval
+        self.feed = False
+        self.interval_cnt = 0
+
+    def update(self, data):
+        self.queue.append(data)
+        if len(self.queue) > self.queue_size:
+            self.queue.pop(0)
+
+    def determine_feed(self):
+        if len(self.queue) == self.queue_size:
+            if self.interval_cnt == self.queue_interval-1:
+                self.interval_cnt = 0
+                self.feed = True
+            else:
+                self.interval_cnt += 1
+                self.feed = False
+        else:
+            self.feed = False
+
+    def get_data(self):
+        if self.feed:
+            return self.queue
+        else:
+            return None
+
+
+
