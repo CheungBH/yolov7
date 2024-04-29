b424dbd3d7c23dcbe785b9209614ae99132f249a
BookerXU <bookerxu@163.com>
2024-04-29 15:54:52 +0800

    debug csv

--- txt2csv.py
+++ txt2csv.py
@@ -11,9 +11,9 @@ val_folder = f"{folder_path}/val"
 
 def write_csv(folder_path_txt, csv_folder):
     if folder_path_txt.split("/")[-1] == "train":
-        csv_path = f"{csv_folder}/train0.csv"
+        csv_path = f"{csv_folder}/train.csv"
     else:
-        csv_path = f"{csv_folder}/val0.csv"
+        csv_path = f"{csv_folder}/val.csv"
 
     with open(csv_path, 'a', newline='', encoding='utf-8') as csvfile:
         csv_writer = csv.writer(csvfile)
@@ -21,26 +21,23 @@ def write_csv(folder_path_txt, csv_folder):
         for file_name in os.listdir(folder_path_txt):
             if file_name.endswith('.txt'):
                 file_path = os.path.join(folder_path_txt, file_name)
-                write_line = []
                 with open(file_path, 'r', encoding='utf-8') as txtfile:
                     for line in txtfile:
-                        class_num = line.split(" ").pop(0)
-                        write_line.append(line.split(" ") + [class_num] + [os.path.basename(file_path)])
-                        new_list = [item for item in write_line[0] if item != '\n']
-                        csv_writer.writerow(new_list)
-
-
-def delete_rows(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
-        reader = csv.reader(infile)
-        writer = csv.writer(outfile)
-        for row in reader:
-            del row[:5]
-            new_row = [cell for index, cell in enumerate(row) if (index + 1) % 3 != 0 and index < 54]
-            writer.writerow(new_row)
-
+                        line_content = line.split(" ")
+                        class_num = int(line_content[0])
+                        boxes = [float(kp) for kp in line_content[1:5]]
+                        kps = [float(kp) for kp in line_content[5:-1]]
+                        normed_kps = []
+                        box_tlw, box_tlh, box_w, box_h = boxes[0]-0.5*boxes[2], boxes[1]-0.5*boxes[3], boxes[2], boxes[3]
+                        for idx, kp in enumerate(kps):
+                            if idx % 3 == 0:
+                                normed_kps.append(str((kp-box_tlw)/box_w) if kp != 0 else 0)
+                            elif idx % 3 == 1:
+                                normed_kps.append(str((kp-box_tlh)/box_h) if kp != 0 else 0)
+                            else:
+                                pass
+                        write_line = normed_kps + [class_num, os.path.basename(file_path)]
+                        csv_writer.writerow(write_line)
 
 write_csv(train_folder, output_csv_folder)
 write_csv(val_folder, output_csv_folder)
-delete_rows(f"{output_csv_folder}/train0.csv", f"{output_csv_folder}/train.csv")
-delete_rows(f"{output_csv_folder}/val0.csv", f"{output_csv_folder}/val.csv")
