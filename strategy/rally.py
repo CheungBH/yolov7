1ed131ed38e6e15579420707ef8d0921987960c8
cheungbh <534660436@qq.com>
2024-11-12 15:46:31 +0800

    add detect strategies

--- strategy/rally.py
+++ strategy/rally.py
@@ -1,2 +1,86 @@
 
+import cv2
+
+
+class RallyChecker:
+    def __init__(self):
+        self.rallying = False
+        self.balls_existing = []
+        self.ball_locations = []
+        self.player_boxes = []
+        self.player_actions = []
+        self.max_no_return_duration = 30
+        self.central_y, self.central_x = 360, 540
+        self.rally_threshold = 0.5
+        self.rally_cnt = 30
+        self.ball_towards = None
+        self.ball_location = None
+        self.recent_ball = 10
+        self.max_ball_change_directio_pixel = 10
+        self.ball_changing_max_threshold = 0.5
+
+    def update_ball_status(self, ball_locations):
+        if ball_locations[0][1] > self.central_y:
+            self.ball_location = "upper"
+        else:
+            self.ball_location = "lower"
+        recent_ball_locations = ball_locations[-self.recent_ball:]
+        ball_y_changing = [ball_locations[idx+1][1] - ball_locations[idx][1]
+                           for idx in range(len(ball_locations))]
+        ball_change_over_threshold = [abs(y_change) > self.max_ball_change_directio_pixel
+                                     for y_change in ball_y_changing]
+        ball_y_changing = [0 if abs(y_change) < self.max_ball_change_directio_pixel else
+                           y_change for y_change in ball_y_changing]
+        if sum(ball_change_over_threshold) / len(ball_change_over_threshold) > self.ball_changing_max_threshold:
+            if sum(ball_y_changing) > 0:
+                ball_towards = "up"
+                if self.ball_towards == "down":
+                    self.rally_cnt += 1
+            else:
+                ball_towards = "down"
+                if self.ball_towards == "up":
+                    self.rally_cnt += 1
+            self.ball_towards = ball_towards
+
+    def check_rally_status(self):
+        if self.rallying:
+            self.check_end_rally()
+        else:
+            self.check_begin_rally()
+
+    def check_begin_rally(self):
+        if sum(self.balls_existing) / len(self.balls_existing) > self.rally_threshold:
+            self.rallying = True
+
+    def check_end_rally(self):
+        if sum(self.balls_existing) / len(self.balls_existing) > self.rally_threshold:
+            self.rallying = False
+
+    def process(self, ball_appears, ball_locations, player_box, player_action):
+        self.balls_existing.append(ball_appears)
+        self.ball_locations.append(ball_locations)
+        for box, action in zip(player_box, player_action):
+            if box[1] > self.central_y:
+                self.player_actions.append(action)
+                self.player_boxes.append(box)
+            else:
+                self.player_actions.append(action)
+                self.player_boxes.append(box)
+        self.check_rally_status()
+        self.update_ball_status(ball_locations)
+
+    def visualize(self, img):
+        h, w = img.shape[:2]
+        cv2.line(img, (self.central_x, 0), (self.central_x, h), (255, 0, 0), 2)
+        cv2.line(img, (0, self.central_y), (w, self.central_y), (255, 0, 0), 2)
+        # Purple
+
+        if self.rallying:
+            cv2.putText(img, "Rallying", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(img, "Not Rallying", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        color = (255, 0, 255)
+        ball_status = "Ball towards: " + str(self.ball_towards) + ", Ball location: " + str(self.ball_location)
+        cv2.putText(img, ball_status, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
 
