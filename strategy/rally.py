b3a5692c9eedec3277871d660e44a7292790458f
cheungbh <534660436@qq.com>
2024-11-13 12:26:09 +0800

    add rally count

--- strategy/rally.py
+++ strategy/rally.py
@@ -9,24 +9,32 @@ class RallyChecker:
         self.ball_locations = []
         self.player_boxes = []
         self.player_actions = []
+
+        self.central_y, self.central_x = 280, 540
         self.max_no_return_duration = 30
-        self.central_y, self.central_x = 360, 540
+
         self.rally_threshold = 0.5
         self.rally_cnt = 0
-        self.ball_towards = ball_init_toward
-        self.ball_location = None
+        self.current_ball_towards = ball_init_toward
+        self.current_ball_position = ball_last_hit
         self.recent_ball = 10
         self.max_ball_change_directio_pixel = 2
         self.ball_changing_max_threshold = 0.5
         self.rally_change_threshold = 3
         self.ball_towards_status = []
-        self.ball_last_hit = ball_last_hit
+        self.ball_position = ball_last_hit
+        self.ball_positions = []
+
+    def check_status(self, status, key):
+        state_ls = [i == key for i in status]
+        ratio = sum(state_ls) / len(state_ls)
+        return ratio >= 0.5
 
     def update_ball_status(self, ball_locations):
-        if ball_locations[1] > self.central_y:
-            self.ball_location = "lower"
-        else:
-            self.ball_location = "upper"
+        ball_location = "lower" if ball_locations[1] > self.central_y else "upper"
+        self.ball_positions.append(ball_location)
+        recent_ball_position = self.ball_positions[-self.recent_ball:]
+
         recent_ball_locations = self.ball_locations[-self.recent_ball:]
         ball_y_changing = [recent_ball_locations[idx+1][1] - recent_ball_locations[idx][1]
                            for idx in range(self.recent_ball-1)]
@@ -34,36 +42,27 @@ class RallyChecker:
                                      for y_change in ball_y_changing]
         ball_y_changing = [0 if abs(y_change) < self.max_ball_change_directio_pixel else
                            y_change for y_change in ball_y_changing]
+        ball_y_changing = ["up" if y_change < 0 else "down" for y_change in ball_y_changing]
         if ball_change_over_threshold:
-            if sum(ball_change_over_threshold) / len(ball_change_over_threshold) > self.ball_changing_max_threshold:
-                ball_towards = "up" if sum(ball_y_changing) < 0 else "down"
-                self.ball_towards_status.append(ball_towards)
-                recent_towards = self.ball_towards_status[-self.recent_ball:]
-                print(recent_towards)
-                ball_towards_up = [ball_towards == "up" for ball_towards in recent_towards]
-                ball_towards_down = [ball_towards == "down" for ball_towards in recent_towards]
-                if sum(ball_towards_up) > sum(ball_towards_down):
-                    if self.ball_towards != "up":
-                        self.ball_towards = "up"
-                        if self.ball_last_hit == "lower":
-                            self.rally_cnt += 1
-                            self.ball_last_hit = "upper"
-                elif sum(ball_towards_down) <= sum(ball_towards_up):
-                    if self.ball_towards != "down":
-                        self.ball_towards = "down"
-                        if self.ball_last_hit == "upper":
-                            self.rally_cnt += 1
-                            self.ball_last_hit = "lower"
+            ball_position_upper_flag = self.check_status(recent_ball_position, "upper")
+            # ball_position_lower_flag = self.check_status(recent_ball_position, "lower")
+            current_ball_position = "upper" if ball_position_upper_flag else "lower"
 
-                        # self.rally_cnt += 1
-                else:
-                    pass
+            ball_towards_up = self.check_status(ball_y_changing, "up")
+            self.current_ball_towards = "up" if ball_towards_up else "down"
+
+            if self.current_ball_towards == "up" and current_ball_position == "upper" and self.ball_position == "lower":
+                self.rally_cnt += 1
+                self.ball_position = "upper"
+            elif self.current_ball_towards == "down" and current_ball_position == "lower" and self.ball_position == "upper":
+                self.rally_cnt += 1
+                self.ball_position = "lower"
+            else:
+                self.ball_position = current_ball_position
 
     def check_rally_status(self):
-        if self.rallying:
-            self.check_end_rally()
-        else:
-            self.check_begin_rally()
+        self.check_end_rally() if self.rallying else self.check_begin_rally()
+
 
     def check_begin_rally(self):
         if sum(self.balls_existing) / len(self.balls_existing) > self.rally_threshold:
@@ -98,7 +97,7 @@ class RallyChecker:
         else:
             cv2.putText(img, "Not Rallying", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         color = (255, 0, 255)
-        ball_status = "Ball towards: " + str(self.ball_towards) + ", Ball location: " + str(self.ball_location)
+        ball_status = "Ball towards: " + str(self.current_ball_towards) + ", Ball location: " + str(self.ball_position)
         cv2.putText(img, ball_status, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
         cv2.putText(img, "Rally count: " + str(self.rally_cnt), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
 
